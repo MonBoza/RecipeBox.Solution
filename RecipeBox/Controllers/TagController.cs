4ac2f5f35ac2bbc5062d4ead17b68b0d1bec5bb2
Monica Barboza <145809292+MonBoza@users.noreply.github.com>
2024-02-13 11:44:51 -0600

    Add Details and Edit actions to TagController
    Co-authored-by: Brianca Knight <briancaknight@gmail.com>

--- RecipeBox/Controllers/TagController.cs
+++ RecipeBox/Controllers/TagController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Threading.Tasks;
 
 namespace RecipeBox.Controllers
 {
@@ -34,6 +35,32 @@ namespace RecipeBox.Controllers
       return RedirectToAction("Index");
     }
 
-    
+    public ActionResult Details(int id)
+    {
+      Tag thisTag = _db.Tags
+                          .Include(tag => tag.JoinEntities)
+                          .ThenInclude(join => join.Recipe)
+                          .FirstOrDefault(tag => tag.TagId == id);
+      return View(thisTag);
+    }
+    public ActionResult Edit(int id)
+    {
+      Tag thisTag = _db.Tags.FirstOrDefault(tags => tags.TagId == id);
+      return View(thisTag);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Tag tag)
+    {
+      _db.Tags.Update(tag);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      Tag = thisTag = _db
+    }
+
   }
 }
